ac03a3c6afa86d051342c71240411d17591da2ab
heorhiisatler <sgv1984@gmail.com>
2021-12-09 17:49:38 +0200

    py fixes

---
+++ API_Request/main.py
@@ -0,0 +1,13 @@
+import requests
+import os
+#import json
+
+username = "sgv1984@gmail.com"
+token = os.environ.get("GITHUB_TOKEN")
+
+r = requests.get("https://api.github.com/user/repos", 
+                 auth=(username, token))
+#print(json.dumps(r.json(), indent=4))
+
+for repo in r.json():
+    print(f"Repo name: {repo['name']}\n Repo URL: {repo['html_url']}\n")
--- Spreadsheet/main.py
+++ Spreadsheet/main.py
@@ -1,6 +1,26 @@
+from os import close
 import openpyxl
+import json
+import codecs
 
-inv_file = openpyxl.load_workbook("D:/Documents/src/training/training-code/py-training/Spreadsheet/mobile_users.xlsx")
+inv_file = openpyxl.load_workbook('./training-code/py-training/'
+                                  'Spreadsheet/mobile_users.xlsx')
 product_list = inv_file["Sheet1"]
+excess_abonents = {}
 
-print(product_list.max_row)
+for mob_abonent_row in range(2, product_list.max_row):
+    #print(product_list.cell(mob_abonent_row, 7).value)
+    abonent_name = product_list.cell(mob_abonent_row, 2).value
+    excess_perc = product_list.cell(mob_abonent_row, 7).value
+    if excess_perc > 0:
+        excess_abonents[abonent_name] = str(excess_perc) + " %"
+    else:
+        None
+
+#print(excess_abonents)
+
+json_object = json.dumps(excess_abonents, indent=5, ensure_ascii=False)
+print(json_object)
+
+with codecs.open("sample.json", "w+", "utf-8") as outfile:
+    outfile.write(json_object)
