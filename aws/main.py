74da5e079a1f80f245cd832a688e1e5683a69396
heorhiisatler <sgv1984@gmail.com>
2021-12-20 13:36:59 +0200

    fixes;)

---
+++ aws/main.py
@@ -0,0 +1,36 @@
+import boto3
+import schedule
+from telegram import t_notify
+
+ec2_client = boto3.client('ec2', region_name='eu-central-1')
+ec2_resourse = boto3.resource('ec2', region_name='eu-central-1')
+
+#instances = ec2_client.describe_instances()
+
+#for rsrv in instances['Reservations']:
+    #print(rsrv['Instances'])
+#    for inst in rsrv['Instances']:
+#        print('Server: {}    State: {}'.format(inst['Tags'][0]['Value'], 
+#                                               inst['State']['Name']))
+
+instance_status = ec2_client.describe_instance_status(
+    IncludeAllInstances=True)
+
+def check_instance_status():
+    t_mes = ''
+    for status in instance_status['InstanceStatuses']:
+        t_mes = t_mes + ('Instance "{}" is {} with state "{}" \n').\
+                         format(status['InstanceId'],
+                         status['InstanceState']['Name'],
+                         status['InstanceStatus']['Status'])
+    print(t_mes)
+    if t_mes == '':
+        t_notify('Error - Empty result')
+    else:
+        t_notify(t_mes)
+
+
+schedule.every(20).seconds.do(check_instance_status)
+
+while True:
+    schedule.run_pending()
